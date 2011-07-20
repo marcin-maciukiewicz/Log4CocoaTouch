f08f24972bda0c1e6ffb1ce8bd45bea31d33cb73
ciukes <mm@bluepocket.com>
2011-07-20 08:53:19 +0100

    Fork from http://sourceforge.net/projects/log4cocoa/

---
+++ Log4CocoaTouch/Log4CocoaTouch.h
@@ -0,0 +1,9 @@
+//
+//  Log4CocoaTouch.h
+//  MSDownloader
+//
+//  Created by Marcin Maciukiewicz on 13/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+#import "NSDate-Calendar.h"
+#import "Log4Cocoa.h"
---
+++ README.md
@@ -0,0 +1,5 @@
+
+This is a fork made from Log4Cocoa project (http://sourceforge.net/projects/log4cocoa/)
+"Touch" flavour added thanks to Jeremy Przasnyski (http://cavoort.com/2010/01/04/log4cocoa-in-an-iphone-project/) 
+
+
